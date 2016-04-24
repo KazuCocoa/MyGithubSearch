529b9d2d3d8971b5f9afb9f8c9fa989d76d3e5ff
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-04-24 23:49:21 +0900

    initial

---
+++ MyGithubSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyGithubSearch
+//
+//  Created by 松尾和昭 on 24/04/2016.
+//  Copyright © 2016 kazucocoa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,16 @@
+platform :ios, '8.0'
+use_frameworks!
+
+pod "AFNetworking"
+
+target 'MyGithubSearch' do
+
+end
+
+target 'MyGithubSearchTests' do
+
+end
+
+target 'MyGithubSearchUITests' do
+
+end
